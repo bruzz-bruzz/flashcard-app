9045d1a1659173e8ccdbf2d3a680479866848b9c
bruzz-bruzz <frkhor@gmail.com>
2026-07-31 21:54:43 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { CookiesProvider } from 'react-cookie'
 import App from './components/App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 createRoot(document.getElementById('root')!).render(
   <CookiesProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/app' element={<App/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </CookiesProvider>
 )
